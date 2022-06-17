c02a0cd07dc37e382f4a58fd0495c314b8ae36e0
dan moore <dmoore@dans-MBP.attlocal.net>
2022-06-17 13:52:26 -0500

    first attempt dynamic inventory

--- .github/actions/ci_deployment_cluster/outputs.tf
+++ .github/actions/ci_deployment_cluster/outputs.tf
@@ -2,3 +2,12 @@ output "private_key" {
   value = "${module.docker_run_bor_test.pk_val}"
   sensitive = true
 }
+
+resource "local_file" "AnsibleInventory" {
+ content = templatefile("ansible_inventory.tmpl",
+ {
+  hostnames         = "${module.docker_run_bor_test.ec2_validator_ips}",
+ }
+ )
+ filename = "../ansible/dynamic_inventory"
+}
