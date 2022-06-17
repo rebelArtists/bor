9b6865ae41046d75241d0fe16ed566f44f7904cf
dan moore <dmoore@dans-MBP.attlocal.net>
2022-06-17 16:01:29 -0500

    cmon

--- .github/actions/ci_deployment_cluster/outputs.tf
+++ .github/actions/ci_deployment_cluster/outputs.tf
@@ -10,7 +10,7 @@ output "host_ips" {
 resource "local_file" "AnsibleInventory" {
  content = templatefile("${path.module}/ansible_inventory.tftpl",
    {
-    hostnames = module.docker_run_bor_test.ec2_validator_ips[*]
+    hostnames = module.docker_run_bor_test.ec2_validator_ips
    }
  )
  filename = "dynamic_inventory"
