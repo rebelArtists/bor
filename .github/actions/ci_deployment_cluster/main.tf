905698a0fd3d9829713bca5395de4a67aff87873
dan moore <dmoore@dans-MBP.attlocal.net>
2022-06-17 11:09:11 -0500

    revise resource names

--- .github/actions/ci_deployment_cluster/main.tf
+++ .github/actions/ci_deployment_cluster/main.tf
@@ -9,11 +9,11 @@ module "docker_run_bor_test" {
   ami                  = "ami-0b9ecb12083282d75"
   instance_type        = "m5d.xlarge"
   instance_port        = 22
-  instance_name_prefix = "node-docker"
+  instance_name_prefix = "node-docker-ci"
   availability_zone    = "us-east-2a"
   ebs_vol_name         = "ebs"
   ebs_vol_size         = 500
-  sec_group_name       = "sentry-networking-docker-ci"
-  key_name             = "aws_key_docker_ci"
+  sec_group_name       = "node-networking-docker-ci"
+  key_name             = "node_docker_ci"
   user_data            = filebase64("${path.module}/user_data.sh")
 }
