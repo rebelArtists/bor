66a53400a726050d88fa060039e4fe38dac96aba
dan moore <dmoore@dans-MBP.attlocal.net>
2022-06-20 10:34:47 -0500

    fix pip

--- .github/actions/ci_deployment_cluster/user_data.sh
+++ .github/actions/ci_deployment_cluster/user_data.sh
@@ -4,10 +4,9 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
+sudo apt-get install python3-pip -y
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
-sudo apt-get install python3-pip -y
 sudo apt install awscli -y
 sudo usermod -aG docker ${USER}
 sudo -s
-sudo apt-get install python3-pip -y
